f29c35874eafc82ae5c1bbb2547787bc98c73cc0
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-20 16:34:01 +0300

    completed task

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,10 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+   def show
+    #find a cheese by its id
+    cheese = Cheese.find_by(id: params[:id]) #prams is a hash of all the parameters passed in the request
+    #send aJson response containing the cheese
+    render json: cheese
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/cheeses', to: 'cheeses#index'
+  get '/cheeses', to: 'cheeses#index' #gets all cheeses from the database
+  get '/cheeses/:id', to: 'cheeses#show' #gets a single cheese from the database
 end
